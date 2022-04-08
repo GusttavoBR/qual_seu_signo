ec6391a4f9fe462e021611bd21599acb721a024d
DanielBarros854 <91908602+DanielBarros854@users.noreply.github.com>
2022-04-07 23:45:03 -0300

    refactor: remove a unnecessary space

--- js/script.js
+++ js/script.js
@@ -1,68 +1,61 @@
-var res = document.querySelector("#res")
+var res = document.querySelector("#res");
 
 function descobrir() {
-    const date = document.querySelector("#date").value
+    const date = document.querySelector("#date").value;
 
-    if (!date) return alert('Você deve digitar uma data valida')
-    const [ano, mes, dia] = new Date(date).toISOString().split("T")[0].split("-")
-
-    console.log(mes)
-
-
+    if (!date) return alert('Você deve digitar uma data valida');
+    const [_ano, mes, dia] = new Date(date).toISOString().split("T")[0].split("-");
 
     switch (mes) {
         case "01":
-            res.innerHTML = Number(dia) <= 20 ? "Capricornio" : "Aquario"
+            res.innerHTML = Number(dia) <= 20 ? "Capricornio" : "Aquario";
             break;
+
         case "02":
-            res.innerHTML = Number(dia) <= 18 ? "Aquario" : "Peixes"
+            res.innerHTML = Number(dia) <= 18 ? "Aquario" : "Peixes";
             break;
 
         case "03":
-            res.innerHTML = Number(dia) <= 20 ? "Peixes" : "Aries"
+            res.innerHTML = Number(dia) <= 20 ? "Peixes" : "Aries";
             break;
 
         case "04":
-            res.innerHTML = Number(dia) <= 20 ? "Aries" : "Touro"
+            res.innerHTML = Number(dia) <= 20 ? "Aries" : "Touro";
             break;
 
         case "05":
-            res.innerHTML = Number(dia) <= 20 ? "Touro" : "Gemeos"
+            res.innerHTML = Number(dia) <= 20 ? "Touro" : "Gemeos";
             break;
 
         case "06":
-            res.innerHTML = Number(dia) <= 20 ? "Gemeos" : "Cancer"
+            res.innerHTML = Number(dia) <= 20 ? "Gemeos" : "Cancer";
             break;
 
         case "07":
-            res.innerHTML = Number(dia) <= 22 ? "Cancer" : "Leão"
+            res.innerHTML = Number(dia) <= 22 ? "Cancer" : "Leão";
             break;
 
         case "08":
-            res.innerHTML = Number(dia) <= 22 ? "Leão" : "Virgem"
+            res.innerHTML = Number(dia) <= 22 ? "Leão" : "Virgem";
             break;
 
         case "09":
-            res.innerHTML = Number(dia) <= 22 ? "Virgem" : "Libra"
+            res.innerHTML = Number(dia) <= 22 ? "Virgem" : "Libra";
             break;
 
         case "10":
-            res.innerHTML = Number(dia) <= 21 ? "Libra" : "Escorpião"
+            res.innerHTML = Number(dia) <= 21 ? "Libra" : "Escorpião";
             break;
 
         case "11":
-            res.innerHTML = Number(dia) <= 21 ? "Escorpião" : "Sargitario"
+            res.innerHTML = Number(dia) <= 21 ? "Escorpião" : "Sargitario";
             break;
 
         case "12":
-            res.innerHTML = Number(dia) <= 20 ? "Sargitario" : "Capricornio"
+            res.innerHTML = Number(dia) <= 20 ? "Sargitario" : "Capricornio";
             break;
-
-        default:
-            break;
-
     }
 }
 
-const date = document.getElementById(`date`)
-date.addEventListener("click", () => { res.innerHTML = `Seu signo é ? ` })
+const date = document.getElementById(`date`);
+date.addEventListener("click", () => { res.innerHTML = `Seu signo é ? ` });
